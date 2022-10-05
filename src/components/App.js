f4d23d68eaf7c439d4ae2114b0155ee619d179a4
Divyansh Dubey <108198974+divyanshdube@users.noreply.github.com>
2022-10-05 13:35:49 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/App.css";
 
 var colors = [
@@ -17,32 +16,34 @@ var colors = [
   "#73A857",
 ];
 
-let Colors;
+let Color;
 
 const App = () => {
-  const [setQuote, isSetQuote] = useState();
-  const [author, isSetAuthor] = useState();
+  const [setquotes, isSetQuotes] = useState();
+  const [setauthor, isSetAuthor] = useState();
 
   const onClickHandler = () => {
     fetch("https://api.quotable.io/random")
       .then((response) => response.json())
       .then((response) => {
-        isSetQuote(response.content);
+        isSetQuotes(response.content);
         isSetAuthor(response.author);
       });
-
-    Colors = parseInt(Math.random() * 12);
-    document.body.style.backgroundColor = colors[Colors];
+    Color = parseInt(Math.random() * 12);
+    document.body.style.backgroundColor = colors[Color];
   };
 
   return (
     <div id="main">
       <div id="wrapper">
         <div id="quote-box">
-          <div id="quote-text">{setQuote}</div>
-          <div className="quote-author">{author}</div>
+          <div className="quote-text">
+            <i>{setquotes}</i>
+          </div>
+          <div className="quote-author">{setauthor}</div>
+
           <div className="buttons">
-            <button className="button" id="new-quote" onClick={onClickHandler}>
+            <button className="button" onClick={onClickHandler} id="new-quote">
               Click Me !
             </button>
           </div>
