fc7aa4f68fb24388ca936c5c45f40746ce854e69
Divyansh Dubey <108198974+divyanshdube@users.noreply.github.com>
2022-10-07 23:31:26 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "../styles/App.css";
-
 var colors = [
-  "#16a085",
-  "#27ae60",
-  "#2c3e50",
-  "#f39c12",
-  "#e74c3c",
-  "#9b59b6",
+  "#16A085",
+  "#27AE60",
+  "#2C3E50",
+  "#F39C12",
+  "#E74C3C",
+  "#9B59B6",
   "#FB6964",
   "#342224",
   "#472E32",
@@ -15,42 +14,38 @@ var colors = [
   "#77B1A9",
   "#73A857",
 ];
-
-let Color;
-
 const App = () => {
-  const [quotes, setQuotes] = useState();
-  const [author, setAuthor] = useState();
-
-  function fetching() {
+  const [quote, setQuote] = React.useState(null);
+  const getQuote = () => {
     fetch("https://api.quotable.io/random")
-      .then((res) => res.json())
-      .then((res) => {
-        setQuotes(res.content);
-        setAuthor(res.author);
-      });
-    Color = parseInt(Math.random() * 12);
-    document.body.style.backgroundColor = colors[Color];
-  }
-
-  return (
-    <div id="main">
-      <div id="wrapper">
-        <div id="quote-box">
-          <div className="quote-text">
-            <i>{quotes}</i>
-          </div>
-          <div className="quote-author">{author}</div>
-
-          <div className="buttons">
-            <button className="button" onClick={fetching} id="new-quote">
-              Click
-            </button>
+      .then((response) => response.json())
+      .then((data) => setQuote(data));
+    document.querySelector("body").style.backgroundColor =
+      colors[Math.floor(Math.random() * colors.length)];
+  };
+  React.useEffect(() => getQuote(), []);
+  console.log(quote);
+  if (quote)
+    return (
+      <div id="main">
+        <div id="wrapper">
+          <div id="quote-box">
+            <div className="quote-text">
+              <i className="fa fa-quote-left"> </i>
+              <span id="text">{quote.content}</span>
+            </div>
+            <div className="quote-author">
+              - <span id="author">{quote.author}</span>
+            </div>
+            <div className="buttons">
+              <button className="button" id="new-quote" onClick={getQuote}>
+                New quote
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  return <div>Loading...</div>;
 };
-
 export default App;
